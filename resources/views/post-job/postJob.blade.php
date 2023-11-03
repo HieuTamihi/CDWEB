0ae7d41ff0619e08e4bc4a4e73a82eec641fbf56
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2023-11-03 14:03:40 +0700

    update

---
+++ resources/views/post-job/postJob.blade.php
@@ -0,0 +1,44 @@
+<x-header></x-header>
+<div class="container">
+    <h2 class="mt-2">Đăng bài tuyển dụng</h2>
+    <div class="bg-secondary p-5 rounded my-4">
+        <form action="" method="">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold required-input">Tiêu đề:</label>
+                        <input type="text" class="form-control" required="">
+                    </div>
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold required-input">Kinh nghiệm làm việc:</label>
+                        <input type="text" class="form-control" required="">
+                    </div>
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold required-input">Kỹ năng lập trình:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold required-input">Loại hình:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3 d-flex"> 
+                        <label class="form-label w-50 font-weight-bold required-input">Yêu cầu:</label>
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold required-input">Mức lương:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3 d-flex">
+                        <label class="form-label w-50 font-weight-bold">Phúc lợi:</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mt-2">
+                <button type="submit" class="btn btn-primary">Đăng bài</button>
+            </div>
+        </form>
+    </div>
+</div>
+<x-footer></x-footer>
