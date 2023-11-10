46d19abd445fac029677e2d7f43daae343fe21b0
Thanh Điền <dienle20192002@gmail.com>
2023-11-10 12:51:58 +0700

    database

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('customer')->insert([
+            [
+                'email' => 'adad@gmail.com',
+                'phone_number' => '0123456789',
+                'full_name' => 'Trần Văn A',
+                'Date' => '2023-11-25',
+                'Address' => 'Võ Văn Ngân, Thủ Đức',
+                'gender' => '1',
+                'status' => '1',              
+            ],
+            [
+                'email' => 'dien@gmail.com',
+                'phone_number' => '0123456559',
+                'full_name' => 'Trần Văn B',
+                'Date' => '2020-11-25',
+                'Address' => 'Thủ Dức',
+                'gender' => '2',
+                'status' => '3',              
+            ],
+            [
+                'email' => 'hello@gmail.com',
+                'phone_number' => '0522622700',
+                'full_name' => 'Trần Văn C',
+                'Date' => '2012-11-25',
+                'Address' => 'Võ Văn Ngân, Thủ Đức',
+                'gender' => '1',
+                'status' => '1',              
+            ],
+            
+        ]);
+    }
+}
---
+++ database/seeders/JobPostingSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class JobPostingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB::table('job_posting')->insert([
+            [
+                'employer_id' => 1,
+                'title' => 'Software Developer (PHP, Lavarel)',
+                'experience' => '1 năm',
+                'type' => 'Junior, Middle, Senior',
+                'skills' => 'Có kinh nghiệm tối thiểu 2 năm lập trình ngôn ngữ PHP.
+                Sử dụng thành thạo cơ sở dữ liệu MySQL, PostgrepSQL.',
+                'required' => 'Khả năng trình bày, giao tiếp: Rõ ràng, logic và khá lưu loát.',
+                'salary' => '1000$',
+            ],
+            [
+                'employer_id' => 2,
+                'title' => 'Java Developer (Junior - Middle)',
+                'experience' => '1 năm, 3 năm',
+                'type' => 'Junior, Middle',
+                'skills' => 'Bachelor or Master degree in Computer Science, IT or related field.
+                1 - 3+ years of experience in Java development or related field.',
+                'required' => 'Develop and maintain high-quality Java applications using modern technologies and best practices.
+                Integrate applications with third-party services and APIs.
+                Design and implement database schemas and queries.',
+                'salary' => '1500$',
+            ],
+            [
+                'employer_id' => 3,
+                'title' => 'Lập Trình Viên Android',
+                'experience' => '06 tháng',
+                'type' => 'Junior',
+                'skills' => 'Kiến thức cơ bản và tư duy về Java/Kotlin, lập trình Android, OOP
+                Đã làm việc hoặc có kinh nghiệm với: Multi Module, MVVM, JetPack, Xml layouts, Compose hoặc các kiến trúc, thiết kế mới',
+                'required' => 'Chủ động, trách nhiệm cao với công việc, khả năng cống hiến và gắn bó lâu dài với Công ty.
+                Đọc hiểu tài liệu Tiếng Anh',
+                'salary' => '2000$',
+            ],
+            [
+                'employer_id' => 4,
+                'title' => 'Devops Engineer (Docker, Kubernetes)',
+                'experience' => '1 năm, 3 năm',
+                'type' => 'Junior, Middle, Senior',
+                'skills' => 'Có hiểu biết ngôn ngữ máy tính: Python, Bash và một điểm cộng nếu Golang, PHP, C#.…
+                Hiểu biết, có khả năng thiết lập và vận hành hệ thống Kubernetes/Docker Container.',
+                'required' => 'Kiến thức về phương pháp phát triển, an ninh mạng và có mối quan tâm sâu sắc đến các công nghệ mới và cải tiến hệ thống nói chung.
+                Đọc hiểu và viết tài liệu chuyên ngành bằng tiếng Anh.',
+                'salary' => '2000$',
+            ],
+            [
+                'employer_id' => 5,
+                'title' => 'Python Developer',
+                'experience' => '1 năm, 3 năm',
+                'type' => 'Middle, Senior',
+                'skills' => '1-3 years of experience in programming Python.
+                Experienced in programming with RDBMS such as MySQL / PostgreSQL databases.',
+                'required' => 'Implement back-end features & business logic, design database & write efficient queries.
+                Build and maintain backend systems for cloud platform projects with Python.',
+                'salary' => '3000$',
+            ],
+            [
+                'employer_id' => 6,
+                'title' => 'Senior Frontend Developer (ReactJS/NextJS)',
+                'experience' => '3 năm',
+                'type' => ' Middle, Senior',
+                'skills' => 'Strong knowledge in Reactjs/Nextjs/Threejs (module, page, client/server side render)
+                Expertise in compiler: Webpack, browserify, npm, yarn',
+                'required' => 'Bachelor’s degree in computer science, Software development, Information technology, or experiences in other related fields, etc. 
+                From 3+ years of frontend software development experience in software/digital product company',
+                'salary' => '1000$',
+            ],
+
+        ]);
+    }
+}
